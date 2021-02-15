0681c8001fe2c1da6f6ebacb5289f3ee69944b90
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2021-02-15 10:39:25 +0330

    Making some changes!

--- Client/wwwroot/js/site.js
+++ Client/wwwroot/js/site.js
@@ -4,24 +4,24 @@
 
 	//alert('Hello, World!');
 
-	alert('Hello, World!')
+	alert(`Hello, World!`)
 
 }
 
 function doSomething2(text1, text2) {
 
-	alert(text1 + ' ' + text2)
+	alert(text1 + ` ` + text2)
 
 }
 
 function doSomething3() {
 
-	return 'Hello, World!'
+	return `Hello, World!`
 
 }
 
 function doSomething4(text1, text2) {
 
-	return text1 + ' ' + text2
+	return text1 + ` ` + text2
 
 }
